9dc869afd1c01122c2d68b6282057728b7f40e62
Mark Raynsford <code@io7m.com>
2017-03-15 10:02:43 +0000

    Use PNG again, crush PNGs, disable all.png

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 universe
 ===
 
-![io7m packages](io7m.svg "io7m packages")
+![io7m packages](io7m.png "io7m packages")
 
-![all packages](all.svg "all packages")
--- graph.sh
+++ graph.sh
@@ -4,5 +4,28 @@ mvn org.kuali.maven.plugins:graph-maven-plugin:1.2.3:dependencies \
   -Dgraph.includes="com.io7m.*" \
   -Dgraph.file="io7m.svg"
 
+inkscape \
+  --export-png io7m.png \
+  --export-background=white \
+  --export-background-opacity=1.0 \
+  --export-area-page \
+  --without-gui \
+  --export-dpi=60 \
+  io7m.svg
+
+pngcrush -rem alla -b io7m.png io7m.png.out
+
+mv io7m.png.out io7m.png
+
 mvn org.kuali.maven.plugins:graph-maven-plugin:1.2.3:dependencies \
   -Dgraph.file="all.svg"
+
+inkscape \
+  --export-png all.png \
+  --export-background=white \
+  --export-background-opacity=1.0 \
+  --export-area-page \
+  --without-gui \
+  --export-dpi=60 \
+  all.svg
+
